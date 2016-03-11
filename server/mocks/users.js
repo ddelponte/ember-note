c31b65be15faab3307db80e52cf6b9869c943466
Dean Del Ponte <dean.delponte@gmail.com>
2016-03-11 10:28:22 -0600

    Fixed bug in users service mock.
    I can now save users!

--- server/mocks/users.js
+++ server/mocks/users.js
@@ -1,19 +1,48 @@
-/*jshint node:true*/
-module.exports = function(app) {
+module.exports = function (app) {
   var express = require('express');
   var usersRouter = express.Router();
 
-  usersRouter.get('/', function(req, res) {
+  // Use the body-parser library in this service
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.json());
+
+  // Create an embedded table using NEDB if it doesn't yet exist
+  var nedb = require('nedb');
+  var userDB = new nedb({filename: 'users', autoload: true});
+
+  // The POST URL is used to create a new record
+  usersRouter.post('/', function (req, res) {
+
+    // Look for the most recently created record and use it to set the id
+    // field of our incoming record, which is required by Ember Data
+    userDB.find({}).sort({id: -1}).limit(1).exec(function (err, users) {
+      if (users.length !== 0)
+        req.body.user.id = users[0].id + 1;
+      else
+        req.body.user.id = 1;
+
+      // Insert the new record into our datastore, and return the newly
+      // created record to Ember Data
+      userDB.insert(req.body.user, function (err, newUser) {
+        res.status(201);
+        res.send(
+          JSON.stringify(
+            {
+              user: newUser
+            }));
+      });
+    });
+  });
+
+  // For now, we won't touch the rest of this code, which was created by
+  // Ember CLI
+  usersRouter.get('/', function (req, res) {
     res.send({
       'users': []
     });
   });
 
-  usersRouter.post('/', function(req, res) {
-    res.status(201).end();
-  });
-
-  usersRouter.get('/:id', function(req, res) {
+  usersRouter.get('/:id', function (req, res) {
     res.send({
       'users': {
         id: req.params.id
@@ -21,27 +50,12 @@ module.exports = function(app) {
     });
   });
 
-  usersRouter.put('/:id', function(req, res) {
-    res.send({
-      'users': {
-        id: req.params.id
-      }
-    });
+  usersRouter.put('/:id', function (req, res) {
   });
 
-  usersRouter.delete('/:id', function(req, res) {
+  usersRouter.delete('/:id', function (req, res) {
     res.status(204).end();
   });
 
-  // The POST and PUT call will not contain a request body
-  // because the body-parser is not included by default.
-  // To use req.body, run:
-
-  //    npm install --save-dev body-parser
-
-  // After installing, you need to `use` the body-parser for
-  // this mock uncommenting the following line:
-  //
-  //app.use('/api/users', require('body-parser').json());
   app.use('/api/users', usersRouter);
 };
